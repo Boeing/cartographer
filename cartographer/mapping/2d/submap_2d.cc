941aa96605a0cc2ba6c5b35ca5d73d33ab5723f4
Alexandre Desbiez <alexandre.desbiez@boeing.com>
2020-01-17 11:19:34 +1100

    submap options now as reference in constructor

--- cartographer/mapping/2d/submap_2d.cc
+++ cartographer/mapping/2d/submap_2d.cc
@@ -79,7 +79,7 @@ proto::SubmapsOptions2D CreateSubmapsOptions2D(
 
 Submap2D::Submap2D(const Eigen::Vector2f& origin, std::unique_ptr<Grid2D> grid,
                    ValueConversionTables* conversion_tables,
-                   const proto::SubmapsOptions2D options)
+                   const proto::SubmapsOptions2D& options)
     : Submap(transform::Rigid3d::Translation(
           Eigen::Vector3d(origin.x(), origin.y(), 0.))),
       conversion_tables_(conversion_tables),
